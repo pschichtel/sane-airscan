af3bc35b020640751a2a0942a08538a5a858f363
Phillip Schichtel <phillip@schich.tel>
2024-02-21 23:39:02 +0100

    Disable IPv6 discovery for now

--- airscan-mdns.c
+++ airscan-mdns.c
@@ -709,35 +709,35 @@ mdns_avahi_browser_callback (AvahiServiceBrowser *b, AvahiIfIndex interface,
         /* Add a device (or lookup for already added) */
         mdns = mdns_finding_get(method, interface, name, initscan);
 
-        /* Initiate resolver -- look for IPv4 addresses */
-        AvahiServiceResolver *r;
+            /* Initiate resolver -- look for IPv4 addresses */
+            AvahiServiceResolver *r;
 
-        r = avahi_service_resolver_new(mdns_avahi_client, interface,
-                protocol, name, type, domain, AVAHI_PROTO_INET, 0,
-                mdns_avahi_resolver_callback, mdns);
+            r = avahi_service_resolver_new(mdns_avahi_client, interface,
+                                           protocol, name, type, domain, AVAHI_PROTO_INET, 0,
+                                           mdns_avahi_resolver_callback, mdns);
 
-        if (r == NULL) {
-            mdns_perror(MDNS_ACTION_RESOLVE, interface, protocol, type, name);
-            mdns_avahi_client_restart_defer();
+            if (r == NULL) {
+                mdns_perror(MDNS_ACTION_RESOLVE, interface, protocol, type, name);
+                mdns_avahi_client_restart_defer();
+                break;
+            }
+
+            mdns->resolvers = ptr_array_append(mdns->resolvers, r);
+
+//            /* Initiate resolver -- look for IPv6 addresses */
+//            r = avahi_service_resolver_new(mdns_avahi_client, interface,
+//                                           protocol, name, type, domain, AVAHI_PROTO_INET6, 0,
+//                                           mdns_avahi_resolver_callback, mdns);
+//
+//            if (r == NULL) {
+//                mdns_perror(MDNS_ACTION_RESOLVE, interface, protocol, type, name);
+//                mdns_avahi_client_restart_defer();
+//                break;
+//            }
+//
+//            /* Attach resolver to device state */
+//            mdns->resolvers = ptr_array_append(mdns->resolvers, r);
             break;
-        }
-
-        mdns->resolvers = ptr_array_append(mdns->resolvers, r);
-
-        /* Initiate resolver -- look for IPv6 addresses */
-        r = avahi_service_resolver_new(mdns_avahi_client, interface,
-                protocol, name, type, domain, AVAHI_PROTO_INET6, 0,
-                mdns_avahi_resolver_callback, mdns);
-
-        if (r == NULL) {
-            mdns_perror(MDNS_ACTION_RESOLVE, interface, protocol, type, name);
-            mdns_avahi_client_restart_defer();
-            break;
-        }
-
-        /* Attach resolver to device state */
-        mdns->resolvers = ptr_array_append(mdns->resolvers, r);
-        break;
 
     case AVAHI_BROWSER_REMOVE:
         mdns = mdns_finding_find(method, interface, name);
